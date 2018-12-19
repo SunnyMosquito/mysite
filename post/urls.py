2d510f6abb3a3f7580ae4fa1beaa08b866218704
SunnyMosquito <awen1018@hotmail.com>
2018-12-19 20:14:16 +0800

    static

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+
+from mysite import settings
+
 from . import views
 
 route = DefaultRouter()
@@ -11,5 +15,7 @@ route.register(r'comments', views.CommentViewSet)
 app_name = 'post'  # 加入了这个后reverse需要加入命名空间
 urlpatterns = [
     path('api/archive/', views.ArchiveView.as_view(), name="archive"),
-    path('api/', include(route.urls))
+    path('api/', include(route.urls)),
+    path('', TemplateView.as_view(template_name='post/index.html',
+                                  extra_context={'settings': settings}))
 ]
